894ada2e63703d85c8f5129c6173f04ecfc86ef9
Oskari Peltonen <oskari.peltonen@outlook.com>
2017-12-04 15:25:24 +0200

    tribe-page new fields + content

--- content/models/tribe_page.py
+++ content/models/tribe_page.py
@@ -4,14 +4,19 @@ from __future__ import unicode_literals
 from django.db import models
 
 from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
 
 
 class TribePage(Page):
-    description = models.CharField(max_length=255, blank=True)
+    body = RichTextField(blank=True)
+    main_lift = RichTextField(blank=True)
+    partners = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
-        FieldPanel('description', classname="full")
+        FieldPanel('body', classname="full"),
+        FieldPanel('main_lift', classname="full"),
+        FieldPanel('partners', classname="full")
     ]
 
 
