ba7e35172f955527e50a5f05e6af347ff555afe3
Oskari Peltonen <oskari.peltonen@outlook.com>
2017-12-07 10:31:20 +0200

    create shopPage model migration

--- content/models/__init__.py
+++ content/models/__init__.py
@@ -1,6 +1,7 @@
 from .teams_page import *
 from .home_page import *
 from .events_page import *
+from .shop_page import *
 from .news_page import *
 from .statistics_page import *
 from .representative_team_page import *
