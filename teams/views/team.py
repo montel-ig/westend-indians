25639d9dc9d0d471741f711b486106dd8c5db6a2
Oskari Peltonen <oskari.peltonen@outlook.com>
2017-12-18 13:35:22 +0200

    added teams_json to teams view

--- teams/views/team.py
+++ teams/views/team.py
@@ -1,6 +1,7 @@
 import json
 
 from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.http import HttpResponse
 
 from teams.models import Team
 
@@ -9,11 +10,14 @@ def teams(request):
     teams = get_list_or_404(Team)
     return render(request, 'teams/teams_page.html', {'teams': teams})
 
+def teams_json(request):
+    teams = json.dumps([team_properties(t) for t in Team.objects.all()])
+    return HttpResponse(teams)
+
 
 def by_slug(request, slug):
     team = get_object_or_404(Team, slug=slug)
     members = members_to_json(team)
-    #team_attributes = team_properties_to_json(team)
     return render(request, 'teams/team.html', locals())
 
 
@@ -34,17 +38,20 @@ def members_to_json(team: Team) -> str:
     return json.dumps(members_dict)
 
 
-def team_properties_to_json(team: Team) -> str:
+def team_properties(team: Team) -> str:
     """ Retrieve a JSON encoded string with the properties of the given team. """
     team_dict = {}
+    #area = team.area
     take_from_team = ('name', 'slug', 'description', 'contact_name', 'contact_email', 'contact_phone',
-                        'leader_name', 'leader_email', 'leader_phone', 'brochure', 'image',
+                        'leader_name', 'leader_email', 'leader_phone',
                         'some_instagram', 'some_twitter', 'some_facebook', 'some_snapchat', 'current_player_count',
-                        'max_player_count', 'gender', 'path', 'sport', 'age_level', 'area')
-    take_from_area = ('name', 'address')
-    for ms in dir(team):
-        #m = ms.member
-        team_dict[ms.id] = dict([(v, getattr(ms, v)) for v in take_from_team])
-        team_dict[ms.id].update(dict([(v, getattr(ms, v)) for v in take_from_area]))
+                        'max_player_count', 'gender', 'path', 'sport', 'age_level',
+                        #"'image'
+                      )
+    take_from_area = ('name', 'address', 'lat', 'lng')
+    for attr in take_from_team:
+        team_dict[attr] = getattr(team, attr)
+    #for attr in take_from_area:
+     #   team_dict[attr] = getattr(area, attr)
 
-    return json.dumps(team_dict)
+    return team_dict
