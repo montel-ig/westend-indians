a31a84f1460b6a5b271609e58b6d60a30c2b6fb1
Oskari Peltonen <oskari.peltonen@outlook.com>
2017-12-21 16:01:46 +0200

    homepage news content is dynamically created from model

--- content/models/indians_base_page.py
+++ content/models/indians_base_page.py
@@ -4,10 +4,12 @@ from __future__ import unicode_literals
 from wagtail.wagtailcore.models import Page
 
 from sponsors.models import Sponsor
+from news.models import NewsItem
 
 
 class IndiansBasePage(Page):
 
+    #sponsors
     @property
     def all_sponsors(self):
         return Sponsor.objects.all()
@@ -24,5 +26,10 @@ class IndiansBasePage(Page):
     def front_page_sponsors(self):
         return Sponsor.objects.filter(visible_for_frontpage=True)
 
+    #news
+    @property
+    def front_page_news(self):
+        return NewsItem.objects.order_by("-created_at")[:3]
+    
     class Meta:
         abstract = True
