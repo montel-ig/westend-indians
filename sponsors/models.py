79fd64bf2d23004f838d582abfa0ee83300021fa
Oskari Peltonen <oskari.peltonen@outlook.com>
2017-12-21 14:29:41 +0200

    add Imagekit to project + set resized images to sponsor-thumbnail. Fixes #2

--- sponsors/models.py
+++ sponsors/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill
 
 class Sponsor(models.Model):
     name = models.CharField(max_length=255)
     url = models.URLField(max_length=255, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    sponsor_thumbnail = ImageSpecField(source='image',
+                                       processors=[ResizeToFill(150, 150)],
+                                       format='PNG',
+                                       options={'quality': 60})
     level = models.CharField(choices=(
         ('top', 'Top level sponsor'),
         ('second', 'Second level sponsor'),
