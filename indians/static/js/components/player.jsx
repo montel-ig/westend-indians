fe360ea0d7a57f1a90a837ff49b35769bb1ae68a
Oskari Peltonen <oskari.peltonen@outlook.com>
2018-01-22 13:08:59 +0200

    adjust player-modal styling, add role

--- indians/static/js/components/player.jsx
+++ indians/static/js/components/player.jsx
@@ -4,16 +4,17 @@ const Player = (props) => {
       <div className="player-img-wrapper">
           <img className={`player-img ${props.image ? 'player-custopm' : 'player-default'} `} src={props.image||props.default_image} />
       </div>
-      {//props.role && <p id="role"> {`#${props.role}`}</p>
-      }
-      <ul className="player-desc">
-        <li>
-          {props.number && <p id="player-number"> {`#${props.number}`}</p>}
-        </li>
-        <li>
-          <p>{props.name}</p>
-        </li>
-      </ul>
+      <div className="desc-container">
+        {props.role && <p id="role"> {`${props.role}`}</p>}
+        <ul className="player-desc">
+          <li>
+            {props.number && <p id="player-number"> {`#${props.number}`}</p>}
+          </li>
+          <li className={props.role ? "employee" : "player"}>
+            <p>{props.name}</p>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 };
