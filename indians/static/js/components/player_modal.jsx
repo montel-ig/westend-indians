215ecfbd6be15c9f3b93481c3759339a575d25a7
Oskari Peltonen <oskari.peltonen@outlook.com>
2018-02-22 12:26:16 +0200

    player-modal design

--- indians/static/js/components/player_modal.jsx
+++ indians/static/js/components/player_modal.jsx
@@ -7,6 +7,9 @@ class PlayerModal extends React.Component {
       isEmployee: typeof props.selectedPlayer.role === 'string'
     }
   }
+  openPage(slug) {
+    window.open(slug, '_blank');
+  }
 
   getYoutubeId(url){
     let ID = '';
@@ -33,46 +36,46 @@ class PlayerModal extends React.Component {
     return (
       <div className={`player-modal-backdrop ${this.state.visibilityClass}`} >
         <div className="player-modal-container">
-          <div className="statistics-container">
-            <div className="upper">
-              <Player_introducing
-                video_url={video_url && this.getYoutubeId(video_url)}
-                image={this.props.selectedPlayer.image}
-                default_image={'/static/images/indians_logo_345x345.jpg'}
-              />
-              <div className="statistics">
-                <p className="player-name">{`${first_name} ${last_name}`}</p>
-                { origin && <p>Kotoisin: {origin}</p> }
-                { number && <p>Pelinumero: {number}</p> }
-                { position && <p>Pelipaikka: {t('player_modal',position)}</p> }
-                { handedness && <p>Kätisyys: {t('player_modal',handedness)}</p> }
-                { born && <p>Syntymävuosi: {born}</p> }
-                { height && <p>Pituus (cm): {height}</p> }
-                { weight && <p>Paino (kg): {weight}</p> }
-                { school && <p>Koulu: {school}</p> }
+          <div className="upper">
+            <Player_introducing
+              video_url={video_url && this.getYoutubeId(video_url)}
+              image={this.props.selectedPlayer.image}
+              default_image={'/static/images/indians_logo_345x345.jpg'}
+            />
+            <div className="middle">
+              <div className={"number"}>
+                { number && <p>#{number}</p> }
+              </div>
+              <div className="name">
+                <div>
+                  <p>{first_name}</p>
+                  <p> {last_name}</p>
+                </div>
               </div>
             </div>
-            <hr />
-            <div className="lower">
-              { role && <p>{role}</p> }
-              <p>{description}</p>
-            </div>
           </div>
-          <ul className="modal-desc">
+          <div className="lower">
+            { role && <p className="position">{role}</p> }
+            { position && <p className="position">{t('player_modal',position)}</p> }
+            { handedness && <p>Kätisyys: {t('player_modal',handedness)}</p> }
+            { height && <p>Pituus (cm): {height}</p> }
+            { weight && <p>Paino (kg): {weight}</p> }
+          </div>
+          <div className="some">
             {this.props.selectedPlayer.some_instagram &&
-              <a href={`https://www.instagram.com/${this.props.selectedPlayer.some_instagram}`}><i className="fa fa-instagram fa-2x" aria-hidden="true"></i></a>}
+              <a href={`https://www.instagram.com/${this.props.selectedPlayer.some_instagram}`} target="_blank"><i className="fab fa-instagram fa-2x" aria-hidden="true"></i></a>}
             {this.props.selectedPlayer.some_snapchat &&
-              <a href={`https://www.snapchat.com/add/${this.props.selectedPlayer.some_snapchat}`}><i className="fa fa-snapchat-square fa-2x" aria-hidden="true"></i></a>}
+              <a href={`https://www.snapchat.com/add/${this.props.selectedPlayer.some_snapchat}`} target="_blank"><i className="fab fa-snapchat-square fa-2x" aria-hidden="true"></i></a>}
             {this.props.selectedPlayer.some_twitter &&
-              <a href={`https://twitter.com/${this.props.selectedPlayer.some_twitter}`}><i className="fa fa-twitter-square fa-2x" aria-hidden="true"></i></a>}
+              <a href={`https://twitter.com/${this.props.selectedPlayer.some_twitter}`} target="_blank"><i className="fab fa-twitter-square fa-2x" aria-hidden="true"></i></a>}
             {this.props.selectedPlayer.some_facebook &&
-              <a href={`https://www.facebook.com/${this.props.selectedPlayer.some_facebook}`}><i className="fa fa-facebook-square fa-2x" aria-hidden="true"></i></a>}
-          </ul>
+              <a href={`https://www.facebook.com/${this.props.selectedPlayer.some_facebook}`} target="_blank"><i className="fab fa-facebook-square fa-2x" aria-hidden="true"></i></a>}
+          </div>
           <div onClick={()=>{this.props.handlePlayerChange("next", this.state.isEmployee)}}>
-            <i className="material-icons right size-125" >navigate_next</i>
+            <i className="icons right fas fa-arrow-right fa-4x"></i>
           </div>
           <div onClick={()=>{this.props.handlePlayerChange(false, this.state.isEmployee)}}>
-            <i className="material-icons left size-125" >navigate_before</i>
+            <i className="icons left fas fa-arrow-left fa-4x"></i>
           </div>
         </div>
       </div>
