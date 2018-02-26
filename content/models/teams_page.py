4e6c6f2a3fd149c80e1936d372721fe99bfd8430
Oskari Peltonen <oskari.peltonen@outlook.com>
2018-02-26 10:41:22 +0200

    richtext field for teamspage description

--- content/models/teams_page.py
+++ content/models/teams_page.py
@@ -5,13 +5,14 @@ from django.db import models
 from content.models.indians_base_page import IndiansBasePage as Parent
 
 from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
 
 from teams.models import Team
 
 
 class TeamsPage(Parent):
-    description = models.CharField(max_length=255, blank=True,)
+    description = RichTextField(blank=True)
     content_panels = Page.content_panels + [
         FieldPanel('description', classname="full")
     ]
