d8b9293b22f284f9fad7461db1af72dd720ed5a4
Oskari Peltonen <oskari.peltonen@outlook.com>
2018-02-27 09:59:39 +0200

    order news items by publication date

--- content/models/indians_base_page.py
+++ content/models/indians_base_page.py
@@ -42,12 +42,12 @@ class IndiansBasePage(Page):
     @property
     def representative_team_page_news(self):
         now = datetime.datetime.now()
-        return NewsItem.objects.filter(subject='miehet-edustus',publication_date__lt=now).order_by("-created_at")[:3]
+        return NewsItem.objects.filter(subject='miehet-edustus',publication_date__lt=now).order_by("-publication_date")[:3]
 
     @property
     def tribe_page_news(self):
         now = datetime.datetime.now()
-        return NewsItem.objects.filter(subject='heimo',publication_date__lt=now).order_by("-created_at")[:3]
+        return NewsItem.objects.filter(subject='heimo',publication_date__lt=now).order_by("-publication_date")[:3]
 
     # Events
 
