916d812cffddd0c1a15d534c5a831114bb90515c
Lauri Kainulainen <lauri.kainulainen@gmail.com>
2018-03-01 12:48:09 +0200

    signups and Signup model for registration

---
+++ signups/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+from teams.models import Team
+
+
+class Signup(models.Model):
+
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.EmailField()
+    phone = models.CharField(max_length=24)
+
+    comments = models.TextField(null=True, blank=True)
+    team = models.ForeignKey(Team)
+
+    staff_comments = models.TextField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
