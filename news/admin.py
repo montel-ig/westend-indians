eba9108cecfa2a4a4f7d30c8bdb7af424aed57c8
Lauri Kainulainen <lauri.kainulainen@gmail.com>
2018-03-14 15:14:27 +0200

    fixed timezone, more vars in news listing

--- news/admin.py
+++ news/admin.py
@@ -5,6 +5,7 @@ from news.models import *
 
 class NewsItemAdmin(admin.ModelAdmin):
     model = NewsItem
+    list_display = ('title', 'publication_date', 'created_at', 'updated_at')
 
 
 admin.site.register(NewsItem, NewsItemAdmin)
