8b621d606f84f9ffa0a48e49ab8b8124eca19a72
Lauri Kainulainen <lauri.kainulainen@gmail.com>
2018-03-20 21:03:17 +0200

    club page has the subnav now, responsive fixes to it

--- indians/templatetags/submenu.py
+++ indians/templatetags/submenu.py
@@ -10,8 +10,8 @@ MENU_HEADER_MATCHER = re.compile(r'\s*<h\d>([^<]+)</h\d>')
 @register.simple_tag()
 def submenu(page):
     menu_items = []
-    for t in ('body', 'tickets', 'information', 'contact'):
-        r = getattr(page, t)
+    for t in ('body', 'introduction', 'tickets', 'values', 'information', 'contact', 'personnel'):
+        r = hasattr(page, t) and getattr(page, t)
         if r:
             f = re.match(MENU_HEADER_MATCHER, r)
             if f:
