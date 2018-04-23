1857485915470ab66d0adbfd8b2f00215f1b1963
Oskari Peltonen <oskari.peltonen@outlook.com>
2018-04-23 09:12:09 +0300

    player modal responsive + youtube view improvements

--- indians/static/js/components/player_modal.jsx
+++ indians/static/js/components/player_modal.jsx
@@ -98,9 +98,9 @@ class PlayerModal extends React.Component {
     this.state.visibilityClass === 'visible' && this.disableScrolling();
     return (
       <div className={`player-modal-backdrop ${this.state.visibilityClass}`} >
-        <div className={`player-modal-container ${video_url ? "wide" : "medium"}`}>
+        <div className="player-modal-container">
           <i id="close">✕</i>
-          <div className="player">
+          <div className={`player ${video_url ? 'wide' : ''}`}>
             <Player_introducing
               video_url={video_url && this.getYoutubeId(video_url)}
               image={this.props.selectedPlayer.image}
@@ -120,7 +120,7 @@ class PlayerModal extends React.Component {
             </div>
           </div>
           <div className="description">
-            <div className="text">
+            <div className={`text ${video_url ? 'more' : ''}`}>
               { position && <p className="position">{t('player_modal',position)}</p> }
               { role && <p className="position">{role}</p> }
               { origin && <p>Kotoisin: {origin}</p> }
@@ -133,7 +133,7 @@ class PlayerModal extends React.Component {
               { game_years_pro_other && <p>Pelivuodet muut: {game_years_pro_other}</p> }
               { yearsCombined > 0 && <p>Pelivuodet yhteensä: {yearsCombined}</p> }
             </div>
-            <div className="bottom">
+            <div className={`bottom ${video_url ? 'fixed-width' : ''}`}>
               <div className="some">
                 {this.props.selectedPlayer.some_instagram &&
                 <a href={`https://www.instagram.com/${this.props.selectedPlayer.some_instagram}`} target="_blank"><svg width="45" height="45" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1152 896q0-106-75-181t-181-75-181 75-75 181 75 181 181 75 181-75 75-181zm138 0q0 164-115 279t-279 115-279-115-115-279 115-279 279-115 279 115 115 279zm108-410q0 38-27 65t-65 27-65-27-27-65 27-65 65-27 65 27 27 65zm-502-220q-7 0-76.5-.5t-105.5 0-96.5 3-103 10-71.5 18.5q-50 20-88 58t-58 88q-11 29-18.5 71.5t-10 103-3 96.5 0 105.5.5 76.5-.5 76.5 0 105.5 3 96.5 10 103 18.5 71.5q20 50 58 88t88 58q29 11 71.5 18.5t103 10 96.5 3 105.5 0 76.5-.5 76.5.5 105.5 0 96.5-3 103-10 71.5-18.5q50-20 88-58t58-88q11-29 18.5-71.5t10-103 3-96.5 0-105.5-.5-76.5.5-76.5 0-105.5-3-96.5-10-103-18.5-71.5q-20-50-58-88t-88-58q-29-11-71.5-18.5t-103-10-96.5-3-105.5 0-76.5.5zm768 630q0 229-5 317-10 208-124 322t-322 124q-88 5-317 5t-317-5q-208-10-322-124t-124-322q-5-88-5-317t5-317q10-208 124-322t322-124q88-5 317-5t317 5q208 10 322 124t124 322q5 88 5 317z" fill="#fff"/></svg></a>}
