424a807cb981eb5ffece6256d8671f3fced80278
Lauri Kainulainen <lauri.kainulainen@gmail.com>
2018-05-02 13:40:21 +0300

    news page pagination and small images

--- news/views.py
+++ news/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.utils.datetime_safe import datetime
 
@@ -7,9 +8,15 @@ from sponsors.models import Sponsor
 
 
 def index(request):
+    highlighted = 3
     news = NewsItem.objects.filter(publication_date__lt=datetime.now(), visible=True).order_by('-publication_date')
-    news_for_carousel_json = _serialize_news(news[0:3])
-    rest_of_news = news[3:]
+    news_for_carousel_json = _serialize_news(news[0:highlighted])
+    page = request.GET.get('page', 1)
+    if page == 0:
+        page = 1
+    rest_of_news = news[highlighted:]
+    paginator = Paginator(rest_of_news, 5)
+    rest_of_news = paginator.page(page)
     sponsors = Sponsor.objects.filter(visible_for_frontpage=True)
     return render(request, 'index.html', locals())
 
