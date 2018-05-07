90ba47206ee047eeb87ddf59a3837c6ee77049ec
Lauri Kainulainen <lauri.kainulainen@gmail.com>
2018-05-07 15:53:31 +0300

    news publication date and fixes to css

--- news/utils.py
+++ news/utils.py
@@ -4,6 +4,7 @@ import json
 def _serialize_news(news):
     return json.dumps(list(
         map(lambda x: {'title': x.title, 'image': x.image_medium.url, 'ingress': x.ingress,
-                       'id': x.id, 'slug': x.slug}, news)
+                       'id': x.id, 'slug': x.slug, 'publication_date': x.publication_date.strftime('%d.%m.%Y')},
+            news)
     ))
 
