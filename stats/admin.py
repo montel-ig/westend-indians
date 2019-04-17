56acc6516bf150f3d2321b05935c744179bbe975
Oskari Peltonen <oskari.peltonen@outlook.com>
2019-04-17 14:50:21 +0300

    enable news-item searching for game report

--- stats/admin.py
+++ stats/admin.py
@@ -4,6 +4,7 @@ from stats.models import *
 
 class PlayedGameAdmin(admin.ModelAdmin):
     list_display = ('home_team', 'away_team', 'date', 'location', 'home_score', 'away_score', 'game_report')
+    raw_id_fields = ('game_report',)
     model = PlayedGame
 
 
