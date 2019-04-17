ca411bd946fd5845def06d7cb7623927ffe5424b
Oskari Peltonen <oskari.peltonen@outlook.com>
2019-04-17 15:30:20 +0300

    newest played games first

--- news/admin.py
+++ news/admin.py
@@ -5,6 +5,7 @@ from news.models import *
 
 class NewsItemAdmin(admin.ModelAdmin):
     list_display = ('title', 'publication_date', 'created_at', 'updated_at')
+    search_fields = ['title']
 
 
 admin.site.register(NewsItem, NewsItemAdmin)
--- stats/admin.py
+++ stats/admin.py
@@ -5,6 +5,7 @@ from stats.models import *
 class PlayedGameAdmin(admin.ModelAdmin):
     list_display = ('home_team', 'away_team', 'date', 'location', 'home_score', 'away_score', 'game_report')
     raw_id_fields = ('game_report',)
+    ordering = ('id',)
     model = PlayedGame
 
 
