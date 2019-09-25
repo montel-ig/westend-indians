23f66246210bd26b2587acef03e78a6c0e13572d
Oskari Peltonen <oskari.peltonen@outlook.com>
2019-09-25 16:57:08 +0300

    allow video autoplay (under browser limitations)

--- indians/static/js/components/player_introducing.jsx
+++ indians/static/js/components/player_introducing.jsx
@@ -5,8 +5,8 @@ const Player_introducing = (props) => {
         if (props.video_url) {
           return <iframe width="600"
                          height="400"
-                         src={`https://www.youtube.com/embed/${props.video_url}?rel=0&amp;controls=0&amp;showinfo=0&autoplay=1&mute=1`}
-                         frameborder="0" allow="autoplay; encrypted-media">
+                         src={`https://www.youtube.com/embed/${props.video_url}?rel=0&amp;controls=1&amp;showinfo=0&autoplay=1&mute=0%loop=1`}
+                         frameBorder="0" allow="autoplay; encrypted-media">
           </iframe>;
         } else if (props.image) {
           return <img className="modal-img player-custom" src={props.image}/>;
