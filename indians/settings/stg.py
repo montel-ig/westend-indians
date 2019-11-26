6d1edb7e699eeeacd445ebb1c6645fe42fbb3925
Muhammad Arslan <m.arslanijaz9@gmail.com>
2019-11-26 21:14:03 +0500

    Testing with temporary domain

--- indians/settings/stg.py
+++ indians/settings/stg.py
@@ -1,5 +1,9 @@
 from .prod import *
 
+# FIXME: Remove ingres temporary domain
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'staging.westendindians.fi', 'westendindians.fi',
+                 'westend-indians-ingres.westend-indians-stage.95.216.142.241.xip.io']
+
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
