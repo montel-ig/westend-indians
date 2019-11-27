e51f837ab468413d61597686273983f7760506a9
Muhammad Arslan <m.arslanijaz9@gmail.com>
2019-11-27 15:10:52 +0500

    Release + Prod deploy

--- indians/settings/stg.py
+++ indians/settings/stg.py
@@ -1,9 +1,5 @@
 from .prod import *
 
-# FIXME: Remove ingres temporary domain
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'staging.westendindians.fi', 'westendindians.fi',
-                 'westend-indians-ingress.westend-indians-stage.95.216.142.241.xip.io']
-
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
