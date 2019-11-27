19296f832ec3ecefd2197f0398387fb4145c0bd3
Muhammad Arslan <m.arslanijaz9@gmail.com>
2019-11-27 13:15:57 +0500

    Fix staging deployment

--- indians/settings/stg.py
+++ indians/settings/stg.py
@@ -2,7 +2,7 @@ from .prod import *
 
 # FIXME: Remove ingres temporary domain
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'staging.westendindians.fi', 'westendindians.fi',
-                 'westend-indians-ingres.westend-indians-stage.95.216.142.241.xip.io']
+                 'westend-indians-ingress.westend-indians-stage.95.216.142.241.xip.io']
 
 CACHES = {
     'default': {
