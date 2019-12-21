ca305ca372e7e71ce20452c1bdc175c5c49089d0
Lauri Kainulainen <lauri.kainulainen@gmail.com>
2019-12-21 12:22:11 +0200

    more thorough checking if the page actually has lifts

--- indians/templatetags/lifts_section.py
+++ indians/templatetags/lifts_section.py
@@ -4,17 +4,19 @@ from content.models import IndiansBasePage
 
 register = template.Library()
 
+
 @register.inclusion_tag('lifts_section.html')
 def lifts_section(page_or_lifts, top=None, bottom=None, bg_color=None):
     lifts = []
     lift_bgcolor = bg_color
     if isinstance(page_or_lifts, IndiansBasePage):
         for attr_prefix in ("pinnalla", "pinnalla2", "pinnalla3"):
-            img = getattr(page_or_lifts, attr_prefix)
-            if img:
+            img = getattr(page_or_lifts, attr_prefix, "").strip()
+            link = getattr(page_or_lifts, attr_prefix+'_link', "").strip()
+            if img and link:
                 lifts.append(dict(
                     image=img,
-                    link=getattr(page_or_lifts, attr_prefix+'_link'),
+                    link=link,
                 ))
         lift_count = page_or_lifts.lift_count
         if not bg_color and hasattr(page_or_lifts, 'lift_bgcolor'):
